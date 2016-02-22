38cb52fee930fd69f3acb673e42695acdebd8229
aro_tech <aro_tech@gmx.com>
2016-02-23 00:52:27 +0100

    Extract methods to reduce cyclomatic complexity and improve readability

--- src/main/java/org/interfaceit/util/ZipFileUtils.java
+++ src/main/java/org/interfaceit/util/ZipFileUtils.java
@@ -28,21 +28,29 @@ public class ZipFileUtils {
 	public static List<String> readFileInZipArchive(File zipFile, String filePathWithinZipStructure)
 			throws IOException {
 		List<String> lines = new ArrayList<>();
-
 		try(ZipFile zf = new ZipFile(zipFile)) {
-			ZipEntry entry = zf.getEntry(filePathWithinZipStructure);
-			if (null != entry) {
-				long size = entry.getSize();
-				if (size > 0) {
-					try (BufferedReader br = new BufferedReader(new InputStreamReader(zf.getInputStream(entry)))) {
-						String line;
-						while ((line = br.readLine()) != null) {
-							lines.add(line);
-						}
-					}
-				}
-			}				
+			readFileLinesIntoList(filePathWithinZipStructure, lines, zf);				
 		}
 		return lines;
+	}
+
+	private static void readFileLinesIntoList(String filePathWithinZipStructure, List<String> lines, ZipFile zf)
+			throws IOException {
+		ZipEntry entry = zf.getEntry(filePathWithinZipStructure);
+		if (null != entry) {
+			readEntryLinesIntoList(lines, zf, entry);
+		}
+	}
+
+	private static void readEntryLinesIntoList(List<String> lines, ZipFile zf, ZipEntry entry) throws IOException {
+		long size = entry.getSize();
+		if (size > 0) {
+			try (BufferedReader br = new BufferedReader(new InputStreamReader(zf.getInputStream(entry)))) {
+				String line;
+				while ((line = br.readLine()) != null) {
+					lines.add(line);
+				}
+			}
+		}
 	}
 }
